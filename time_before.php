08c2dac6d113699dacc91708a749734df6801d0a
Luke Stevenson <lucanos@gmail.com>
2016-01-22 22:46:07 +1100

    Extended Formatting Options

--- time_before.php
+++ time_before.php
@@ -5,6 +5,11 @@
   define( TIMEBEFORE_MINUTES,     '{num} minutes ago' );
   define( TIMEBEFORE_HOUR,        '{num} hour ago' );
   define( TIMEBEFORE_HOURS,       '{num} hours ago' );
+  define( TIMEBEFORE_DAYS,        '{num} days ago' );
+  define( TIMEBEFORE_WEEK,        '{num} week ago' );
+  define( TIMEBEFORE_WEEKS,       '{num} weeks ago' );
+  define( TIMEBEFORE_MONTH,       '{num} month ago' );
+  define( TIMEBEFORE_MONTHS,      '{num} months ago' );
   define( TIMEBEFORE_YESTERDAY,   'yesterday' );
   define( TIMEBEFORE_FORMAT,      '%e %b' );
   define( TIMEBEFORE_FORMAT_YEAR, '%e %b, %Y' );
@@ -26,6 +31,15 @@
 
     elseif( $diff < 3600 * 24 * 2 ) // it happened yesterday
       return TIMEBEFORE_YESTERDAY;
+
+    elseif( $diff < 3600 * 24 * 7 )
+      return str_replace( '{num}', round( $diff / ( 3600 * 24 ) ), TIMEBEFORE_DAYS );
+
+    elseif( $diff < 3600 * 24 * 7 * 4 )
+      return str_replace( '{num}', ( $out = round( $diff / ( 3600 * 24 * 7 ) ) ), $out == 1 ? TIMEBEFORE_WEEK : TIMEBEFORE_WEEKS );
+
+    elseif( $diff < 3600 * 24 * 7 * 4 * 12 )
+      return str_replace( '{num}', ( $out = round( $diff / ( 3600 * 24 * 7 * 4 ) ) ), $out == 1 ? TIMEBEFORE_MONTH : TIMEBEFORE_MONTHS );
 
     else // falling back on a usual date format as it happened later than yesterday
       return strftime( date( 'Y', $time ) == date( 'Y' ) ? TIMEBEFORE_FORMAT : TIMEBEFORE_FORMAT_YEAR, $time );
