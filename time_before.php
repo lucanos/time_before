834055f898512b43bc4f4a2ac333ae50f3cc2940
Luke Stevenson <lucanos@gmail.com>
2016-01-22 23:00:47 +1100

    Update time_before.php

--- time_before.php
+++ time_before.php
@@ -1,46 +1,75 @@
 <?php
 
-  define( TIMEBEFORE_NOW,         'now' );
-  define( TIMEBEFORE_MINUTE,      '{num} minute ago' );
-  define( TIMEBEFORE_MINUTES,     '{num} minutes ago' );
-  define( TIMEBEFORE_HOUR,        '{num} hour ago' );
-  define( TIMEBEFORE_HOURS,       '{num} hours ago' );
-  define( TIMEBEFORE_DAYS,        '{num} days ago' );
-  define( TIMEBEFORE_WEEK,        '{num} week ago' );
-  define( TIMEBEFORE_WEEKS,       '{num} weeks ago' );
-  define( TIMEBEFORE_MONTH,       '{num} month ago' );
-  define( TIMEBEFORE_MONTHS,      '{num} months ago' );
-  define( TIMEBEFORE_YESTERDAY,   'yesterday' );
-  define( TIMEBEFORE_FORMAT,      '%e %b' );
-  define( TIMEBEFORE_FORMAT_YEAR, '%e %b, %Y' );
+define( TIMEBEFORE_NOW,         'now' );
+define( TIMEBEFORE_MINUTE,      '{num} minute ago' );
+define( TIMEBEFORE_MINUTES,     '{num} minutes ago' );
+define( TIMEBEFORE_HOUR,        '{num} hour ago' );
+define( TIMEBEFORE_HOURS,       '{num} hours ago' );
+define( TIMEBEFORE_YESTERDAY,   'yesterday' );
+define( TIMEBEFORE_DAYS,        '{num} days ago' );
+define( TIMEBEFORE_WEEK,        '{num} week ago' );
+define( TIMEBEFORE_WEEKS,       '{num} weeks ago' );
+define( TIMEBEFORE_MONTH,       '{num} month ago' );
+define( TIMEBEFORE_MONTHS,      '{num} months ago' );
+define( TIMEBEFORE_FORMAT,      '%e %b' );
+define( TIMEBEFORE_FORMAT_YEAR, '%e %b, %Y' );
 
-  function time_ago( $time )
-  {
-    $out    = ''; // what we will print out
-    $now    = time(); // current time
-    $diff   = $now - $time; // difference between the current and the provided dates
+function time_ago( $time ){
 
-    if( $diff < 60 ) // it happened now
-      return TIMEBEFORE_NOW;
+  $out    = ''; // what we will print out
+  $now    = time(); // current time
+  $diff   = $now - $time; // difference between the current and the provided dates
 
-    elseif( $diff < 3600 ) // it happened X minutes ago
-      return str_replace( '{num}', ( $out = round( $diff / 60 ) ), $out == 1 ? TIMEBEFORE_MINUTE : TIMEBEFORE_MINUTES );
+  if( $diff < 60 ) // it happened now
+    return TIMEBEFORE_NOW;
 
-    elseif( $diff < 3600 * 24 ) // it happened X hours ago
-      return str_replace( '{num}', ( $out = round( $diff / 3600 ) ), $out == 1 ? TIMEBEFORE_HOUR : TIMEBEFORE_HOURS );
+  elseif( $diff < 3600 ) // it happened X minutes ago
+    return str_replace( '{num}', ( $out = round( $diff / 60 ) ), $out == 1 ? TIMEBEFORE_MINUTE : TIMEBEFORE_MINUTES );
 
-    elseif( $diff < 3600 * 24 * 2 ) // it happened yesterday
-      return TIMEBEFORE_YESTERDAY;
+  elseif( $diff < 3600 * 24 ) // it happened X hours ago
+    return str_replace( '{num}', ( $out = round( $diff / 3600 ) ), $out == 1 ? TIMEBEFORE_HOUR : TIMEBEFORE_HOURS );
 
-    elseif( $diff < 3600 * 24 * 7 )
-      return str_replace( '{num}', round( $diff / ( 3600 * 24 ) ), TIMEBEFORE_DAYS );
+  elseif( $diff < 3600 * 24 * 2 ) // it happened yesterday
+    return TIMEBEFORE_YESTERDAY;
 
-    elseif( $diff < 3600 * 24 * 7 * 4 )
-      return str_replace( '{num}', ( $out = round( $diff / ( 3600 * 24 * 7 ) ) ), $out == 1 ? TIMEBEFORE_WEEK : TIMEBEFORE_WEEKS );
+  elseif( $diff < 3600 * 24 * 7 )
+    return str_replace( '{num}', round( $diff / ( 3600 * 24 ) ), TIMEBEFORE_DAYS );
 
-    elseif( $diff < 3600 * 24 * 7 * 4 * 12 )
-      return str_replace( '{num}', ( $out = round( $diff / ( 3600 * 24 * 7 * 4 ) ) ), $out == 1 ? TIMEBEFORE_MONTH : TIMEBEFORE_MONTHS );
+  elseif( $diff < 3600 * 24 * 7 * 4 )
+    return str_replace( '{num}', ( $out = round( $diff / ( 3600 * 24 * 7 ) ) ), $out == 1 ? TIMEBEFORE_WEEK : TIMEBEFORE_WEEKS );
 
-    else // falling back on a usual date format as it happened later than yesterday
-      return strftime( date( 'Y', $time ) == date( 'Y' ) ? TIMEBEFORE_FORMAT : TIMEBEFORE_FORMAT_YEAR, $time );
+  elseif( $diff < 3600 * 24 * 7 * 4 * 12 )
+    return str_replace( '{num}', ( $out = round( $diff / ( 3600 * 24 * 7 * 4 ) ) ), $out == 1 ? TIMEBEFORE_MONTH : TIMEBEFORE_MONTHS );
+
+  else // falling back on a usual date format as it happened later than yesterday
+    return strftime( date( 'Y', $time ) == date( 'Y' ) ? TIMEBEFORE_FORMAT : TIMEBEFORE_FORMAT_YEAR, $time );
+}
+
+function time_before_js_to_string( $constant_name ){
+  $c = @constant( $constant_name );
+  if( !is_null( $c ) )
+    echo 'var '.$constant_name.' = '.json_encode( $c ).';';
+}
+
+function time_before_js(){
+  echo '<script>';
+  $templates = array(
+    'TIMEBEFORE_NOW' ,
+    'TIMEBEFORE_MINUTE' ,
+    'TIMEBEFORE_MINUTES' ,
+    'TIMEBEFORE_HOUR' ,
+    'TIMEBEFORE_HOURS' ,
+    'TIMEBEFORE_YESTERDAY' ,
+    'TIMEBEFORE_DAYS' ,
+    'TIMEBEFORE_WEEK' ,
+    'TIMEBEFORE_WEEKS' ,
+    'TIMEBEFORE_MONTH' ,
+    'TIMEBEFORE_MONTHS' ,
+    'TIMEBEFORE_FORMAT' ,
+    'TIMEBEFORE_FORMAT_YEAR'
+  );
+  foreach( $templates as $t ){
+    time_before_js_to_string( $t );
   }
+  echo '</script>';
+}
